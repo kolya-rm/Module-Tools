b09325ebc42063bad82084c2324496e91a474cfa
Nikolai Romanov <kolya.rm@gmail.com>
2026-03-24 10:18:21 +0100

    Updated script in the script-02.sh file to print string without quote characters.

--- jq/script-02.sh
+++ jq/script-02.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 # TODO: Write a command to output the address of the person, all on one line, with a comma between each line.
 # Your output should be exactly the string "35 Fashion Street, London, E1 6PX", but should not contain any quote characters.
 
-cat person.json | jq '.address | join(", ")'
+cat person.json | jq -r '.address | join(", ")'
