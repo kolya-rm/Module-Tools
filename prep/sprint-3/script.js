993a62d43e165bbf1ab364c334b44c857a39a1b3
Nikolai Romanov <kolya.rm@gmail.com>
2026-03-25 09:44:26 +0100

    Modefied script to use program from commander module. Added input for a particular char to search.

--- prep/sprint-3/script.js
+++ prep/sprint-3/script.js
@@ -1,20 +1,31 @@
+import { program } from "commander";
 import process from "node:process";
 import { promises as fs } from "node:fs";
-import { program } from "commander";
 
-const argv = process.argv.slice(2);
+program
+  .name("count-containing-words")
+  .description("Counts words in a file that contain a particular character")
+  .option("-c, --char <char>", "The character for search", "e")
+  .argument("<path>", "The file path to process")
+
+program.parse();
+
+const argv = program.args;
 
 if (argv.length != 1) {
-  console.error(`Expected exactly 1 argument (a path) to be passed but got ${argv.length}`);
+  console.error(
+    `Expected exactly 1 argument (a path) to be passed but got ${argv.length}`,
+  );
   process.exit(1);
 }
 
 const path = argv[0];
+const char = program.opts().char;
 
 const content = await fs.readFile(path, "utf-8");
 const countOfWordsContainingEs = content
   .split(" ")
-  .filter(word => word.includes("e"))
+  .filter((word) => word.includes(char))
   .length;
 
-  console.log(countOfWordsContainingEs);
+console.log(countOfWordsContainingEs);
