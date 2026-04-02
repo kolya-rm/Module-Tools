2451645014874fcb266238941d66949e97e8878f
Nikolai Romanov <kolya.rm@gmail.com>
2026-04-02 20:03:55 +0200

    Implemented basic logic outputs file content to stdout.

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,5 +1,6 @@
 import { program } from "commander";
 import { promises as fs } from "node:fs";
+import { constants } from "node:fs";
 import process from "node:process";
 
 program
@@ -10,3 +11,17 @@ program
 program.parse();
 
 const argv = program.args;
+
+printFile(argv[0]);
+
+function printFile(path) {
+  fs.access(path, constants.R_OK).then(() => {
+    fs.readFile(path, "utf8").then( data => {
+      console.log(data.toString().trim());
+    }).catch(error => {
+      console.error(`cat.js: ${path}: ${error}}`);
+    })
+  }).catch(() => {
+    console.error(`cat.js: ${path}: No such file or directory`);
+  })
+}
