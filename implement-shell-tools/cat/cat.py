f93112decc07575159439a01a284d274a3438a05
Nikolai Romanov <kolya.rm@gmail.com>
2026-04-12 19:46:29 +0200

    cat emulating: implemented reading and printing multiple files

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -1,17 +1,21 @@
+import sys
 import argparse
+
 
 parser = argparse.ArgumentParser(
   prog = "concatenate-and-print-files",
   description = "The cat utility reads files sequentially, writing them to the standard output."
 )
 
-parser.add_argument("path", help = "The file to read and print")
+parser.add_argument("path", help = "The file to read and print", nargs = "+")
 
 args = parser.parse_args()
 
+
 def readAndPrintFile():
   buffer = []
-  buffer += readFile(args.path)
+  for path in args.path:
+    buffer.append(readFile(path))
   printBuffer(buffer)
 
 def readFile(path):
@@ -20,10 +24,17 @@ def readFile(path):
       content = file.read()
     return content.strip().split("\n")
   except:
-    return  ["cat.js: " + path + ": No such file or directory"]
+    return  ["cat.py: " + path + ": No such file or directory"]
   
 def printBuffer(buffer):
-  for content in buffer:
-    print(content)
+  for fileContent in buffer:
+    printContent(fileContent)
+
+def printContent(fileContent):
+  for string in fileContent:
+    if string.startswith("cat.py:"):
+      print(string, file = sys.stderr)
+    else:
+      print(string)
 
 readAndPrintFile()
