9b81dbfc9da6df4633daec570a9271a908c4a25b
Nikolai Romanov <kolya.rm@gmail.com>
2026-04-12 19:28:50 +0200

    cat emulating. implemented single file read and print.

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -9,4 +9,21 @@ parser.add_argument("path", help = "The file to read and print")
 
 args = parser.parse_args()
 
-print(args.path)
+def readAndPrintFile():
+  buffer = []
+  buffer += readFile(args.path)
+  printBuffer(buffer)
+
+def readFile(path):
+  try:
+    with open(path, "r") as file:
+      content = file.read()
+    return content.strip().split("\n")
+  except:
+    return  ["cat.js: " + path + ": No such file or directory"]
+  
+def printBuffer(buffer):
+  for content in buffer:
+    print(content)
+
+readAndPrintFile()
